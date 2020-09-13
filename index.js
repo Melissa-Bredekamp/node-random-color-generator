bb56f72e50056ba747b976aab57426888c6eded2
Melissa Bredekamp <melissabredekamp@Melissas-MacBook-Air.local>
2020-09-13 11:41:21 +0200

    First commit

--- index.js
+++ index.js
@@ -1,30 +1,4 @@
 const randomColor = require('randomcolor');
-//  console.log(randomColor());
-// const characters = '#';
-
-// console.log('#');
-// console.log('########################');
-// console.log('########################');
-// console.log('########################');
-// console.log('########################');
-// console.log('########################');
-// console.log('########################');
-// console.log('########', '', '#########');
-// console.log('########', 'randomcolor', '#########');
-// console.log('########', randomColor(), '########'); */
-
-// const blockOfCharacters = () => {
-//   {
-//     console.log('##############################'),
-//       console.log('##############################'),
-//       console.log('##############################'),
-//       console.log(`###### ${randomColor()} #####`),
-//       console.log('##############################'),
-//       console.log('##############################'),
-//       console.log('##############################');
-//   }
-// };
-// console.log(blockOfCharacters);
 
 const hue = process.argv[2];
 // console.log(hue);
@@ -32,27 +6,16 @@ const luminosity = process.argv[3];
 // console.log(luminosity);
 
 const color = randomColor({ hue: hue, luminosity: luminosity });
-console.log(color);
+// console.log(color);
 
 const chalk = require('chalk');
-// console.log(chalk.green('##########'));
-// console.log(chalk.green('###########   ' + color + '    ##########'));
-
-// for (var i = 0; i < 5; i++) {
-// console.log('Hi');
-// }
-
-// for (var i = 0; i < 3; i++) {
-// console.log('###############################');
-// }
-for (var i = 0; i < 3; i++) {
-  console.log(chalk.green('##############################'));
-}
 
 for (var i = 0; i < 3; i++) {
-  console.log(chalk.green('#########  ' + color + '  ##########'));
+  console.log(chalk.hex(color)('##############################'));
 }
+
+console.log(chalk.hex(color)('#########  ' + color + '  ##########'));
 
 for (var j = 0; j < 3; j++) {
-  console.log(chalk.green('##############################'));
+  console.log(chalk.hex(color)('##############################'));
 }
