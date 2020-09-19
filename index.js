0efcd6292a02954afe73410179566cc1676a923f
melissabredekamp <mbredekamp@gmail.com>
2020-09-19 19:30:13 +0200

    Added README.md and start stretch goals

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 const randomColor = require('randomcolor');
+const chalk = require('chalk');
 
 const hue = process.argv[2];
 // console.log(hue);
 const luminosity = process.argv[3];
 // console.log(luminosity);
-
 const color = randomColor({ hue: hue, luminosity: luminosity });
 // console.log(color);
 
-const chalk = require('chalk');
+// Stretch goals:
+// function askFunction() {
+//   if (typeof hue != 'undefined') {
+//     ask1 = 'What hue are you searching for?';
+//     console.log(ask1);
+//     //   } else if (typeof luminosity != 'undefined') {
+//     //     ask2 = 'what luminosity are you searching for?';
+//     //     console.log(ask2);
+//     //   } else {
+    for (var i = 0; i < 3; i++) {
+      console.log(chalk.hex(color)('##############################'));
+    }
 
-function askFunction() {
-  if (typeof hue != 'red') {
-    ask1 = 'What hue are you searching for?';
-    console.log(ask1);
+    console.log(chalk.hex(color)('#########  ' + color + '  ##########'));
+
+    for (var j = 0; j < 3; j++) {
+      console.log(chalk.hex(color)('##############################'));
+    }
   }
-  
-  else if (typeof luminosity != 'undefined)' {
-    ask2 = 'what luminosity are you searching for?';
-    console.log(ask2);
-  }
-
-else {
-  for (var i = 0; i < 3; i++) {
-  console.log(chalk.hex(color)('##############################'));
-}
-
-console.log(chalk.hex(color)('#########  ' + color + '  ##########'));
-
-for (var j = 0; j < 3; j++) {
-  console.log(chalk.hex(color)('##############################'));
-}};
 }
